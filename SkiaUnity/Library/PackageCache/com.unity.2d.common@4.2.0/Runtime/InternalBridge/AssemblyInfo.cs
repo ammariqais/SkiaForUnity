9044b41b25c167fc9cdaa5e7ec80a1a775ee91ff
ammariqais <ammariqais66@gmail.com>
2023-07-07 03:39:36 +0300

    remove ignore folders

--- SkiaUnity/Library/PackageCache/com.unity.2d.common@4.2.0/Runtime/InternalBridge/AssemblyInfo.cs
+++
@@ -1,6 +0,0 @@
-using System.Runtime.CompilerServices;
-
-[assembly: InternalsVisibleTo("Unity.2D.Animation.Runtime")]
-[assembly: InternalsVisibleTo("Unity.2D.Animation.Editor")]
-[assembly: InternalsVisibleTo("Unity.2D.Animation.Tests.RuntimeTests")]
-[assembly: InternalsVisibleTo("Unity.2D.IK.Editor")]
