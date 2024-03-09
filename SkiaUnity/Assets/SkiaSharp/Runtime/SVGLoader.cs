000cb178b91ad3db20bdf1cb3812af4621f91359
Qais Ammari <62248657+ammariqais@users.noreply.github.com>
2024-03-09 03:52:57 +0300

    fix isExternalInit (#43)

--- SkiaUnity/Assets/SkiaSharp/Runtime/SVGLoader.cs
+++ SkiaUnity/Assets/SkiaSharp/Runtime/SVGLoader.cs
@@ -5,38 +5,34 @@ using Svg.Skia;
 using UnityEngine.UI;
 
 namespace SkiaSharp.Unity {
-    public class SVGLoader : MonoBehaviour {
-        [SerializeField]
-        private TextAsset svgFile;
+  public class SVGLoader : MonoBehaviour {
+    [SerializeField]
+    private TextAsset svgFile;
         
-        private SKBitmap bmp;
-        private SKSurface surface;
-        private Texture2D texture;
-        private RawImage rawImage;
+    private SKBitmap bmp;
+    private SKSurface surface;
+    private Texture2D texture;
+    private RawImage rawImage;
 
 
-        
-        // Start is called before the first frame update
-        void Start() {
-            rawImage = GetComponent<RawImage>();
+    void Start() {
+      rawImage = GetComponent<RawImage>();
             
-            using (var svg = new SKSvg())
-            {
-                if (svg.FromSvg(svgFile.text) is { })
-                {
-                    bmp = svg.Picture.ToBitmap(SKColor.Empty, 1, 1, SKColorType.Rgba8888, SKAlphaType.Opaque, null);
-                    surface = SKSurface.Create(bmp.Info); 
-                    SKCanvas canvas = new SKCanvas(bmp);
-                   canvas.DrawPicture(svg.Picture);
-                   TextureFormat format = (bmp.Info.ColorType == SKColorType.Rgba8888) ? TextureFormat.RGBA32 : TextureFormat.BGRA32;
-                   texture = new Texture2D(bmp.Info.Width, bmp.Info.Height, format, false);
-                   texture.wrapMode = TextureWrapMode.Repeat;
-                   var pixmap = surface.PeekPixels();
-                   texture.LoadRawTextureData(bmp.GetPixels(), pixmap.RowBytes * pixmap.Height);
-                   texture.Apply();
-                   rawImage.texture = texture;
-                }
-            }
+      using (var svg = new SKSvg())
+      {
+        if (svg.FromSvg(svgFile.text) is { }) { 
+          bmp = svg.Picture.ToBitmap(SKColor.Empty, rawImage.GetComponent<RectTransform>().sizeDelta.x/svg.Picture.CullRect.Width, rawImage.GetComponent<RectTransform>().sizeDelta.y/ svg.Picture.CullRect.Height, SKColorType.Rgba8888, SKAlphaType.Opaque, null);
+          surface = SKSurface.Create(bmp.Info); 
+          SKCanvas canvas = new SKCanvas(bmp);
+          TextureFormat format = (bmp.Info.ColorType == SKColorType.Rgba8888) ? TextureFormat.RGBA32 : TextureFormat.BGRA32;
+          texture = new Texture2D(bmp.Info.Width, bmp.Info.Height, format, false);
+          texture.wrapMode = TextureWrapMode.Repeat;
+          var pixmap = surface.PeekPixels();
+          texture.LoadRawTextureData(bmp.GetPixels(), pixmap.RowBytes * pixmap.Height);
+          texture.Apply();
+          rawImage.texture = texture;
         }
+      }
     }
+  }
 }
--- SkiaUnity/Assets/SkiaSharp/SkiaSharp-Bindings/Svg.Model/IsExternalInit.cs
+++ SkiaUnity/Assets/SkiaSharp/SkiaSharp-Bindings/Svg.Model/IsExternalInit.cs
@@ -1,7 +1,5 @@
-﻿#if NET461 || NETSTANDARD
-// ReSharper disable once CheckNamespace
+﻿// ReSharper disable once CheckNamespace
 namespace System.Runtime.CompilerServices {
   internal static class IsExternalInit {
   }
 }
-#endif
